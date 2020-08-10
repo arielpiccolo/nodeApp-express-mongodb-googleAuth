99a71526cc583e01a304f19b54d95049d101f216
arielpiccolo <arielpiccolo@gmail.com>
2020-08-10 11:28:13 +0100

    google auth successful and user data stored in mongodb

--- config/passport.js
+++ config/passport.js
@@ -7,12 +7,40 @@ module.exports = function(passport) {
     passport.use(new GoogleStrategy ({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
+        callbackURL: '/auth/google/callback',
     },
 
     async (accessToken, refreshToken, profile, done) => {
-        console.log(profile)
+        // console.log(profile)
+        // account details were returned successfully to the console,now we can take those details and feed the database with them
+        // now lets create a new object that makes our database schema and fill them with googles returned values.
+        const newUser = {
+            googleId: profile.id,
+            displayName: profile.displayName,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            image: profile.photos[0].value
+
         }
+        // lets create a try and catch
+        try {
+            // try to match googleId and profile.id
+            let user = await User.findOne({ googleId: profile.id})
+
+            // if there is a match
+            if(user) {
+                done(null,user)
+                // if not return schema and create one.
+            } else {
+                user = await User.create(newUser)
+                done(null, user)
+            }
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+        
     )
   )
 
