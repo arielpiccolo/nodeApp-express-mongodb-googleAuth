879c201e4e669f66d88b7841916704c0eb19a965
arielpiccolo <arielpiccolo@gmail.com>
2020-08-13 23:35:20 +0100

    add new story button created and also the form to enter text and selector for public or private stories.

---
+++ routes/stories.js
@@ -0,0 +1,17 @@
+// create route for add.hbs(add new story form)
+
+const express = require('express');
+const route = express.Router();
+const { ensureAuth } = require('../middleware/auth')
+
+const Story = require('../models/Story')
+
+
+// @desc  Show add page
+// @route GET /stories/add
+route.get('/add', ensureAuth, (req, res) => {
+    res.render('stories/add')
+});
+
+
+module.exports = route
