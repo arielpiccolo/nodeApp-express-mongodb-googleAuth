290364f02bde11cef3ba1174de58100dc1546463
Ariel Piccolo <47399439+arielpiccolo@users.noreply.github.com>
2020-08-20 14:54:04 +0100

    several updates

--- routes/stories.js
+++ routes/stories.js
@@ -7,11 +7,18 @@ const { ensureAuth } = require('../middleware/auth')
 const Story = require('../models/Story')
 
 
+
+
+
 // @desc  Show add page
 // @route GET /stories/add
 route.get('/add', ensureAuth, (req, res) => {
     res.render('stories/add')
 });
+
+
+
+
 
 
 module.exports = route
